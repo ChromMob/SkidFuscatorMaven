afdfea962a52ce3c63b4e5b9322855d0208d54a3
ChromMob <modern.nej@gmail.com)>
2023-01-09 00:03:55 +0100

    Remove useless variable now that we dont print console

--- src/main/java/me/chrommob/skidfuscatormaven/SkidFuscatorMaven.java
+++ src/main/java/me/chrommob/skidfuscatormaven/SkidFuscatorMaven.java
@@ -73,9 +73,8 @@ public class SkidFuscatorMaven extends AbstractMojo {
                 throw new RuntimeException(e);
             }
             System.out.println("Obfuscating: " + outPutFile.getName());
-            Process process;
             try {
-                process = Runtime.getRuntime().exec("java -jar " + skidfuscatorJar.getAbsolutePath() + " " + outputFolder + File.separator + outPutFile.getName() + " -li=" + new File(skidfuscatorFolder + File.separator + "libs"), new String[0], outputFolder);
+                Runtime.getRuntime().exec("java -jar " + skidfuscatorJar.getAbsolutePath() + " " + outputFolder + File.separator + outPutFile.getName() + " -li=" + new File(skidfuscatorFolder + File.separator + "libs"), new String[0], outputFolder);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
